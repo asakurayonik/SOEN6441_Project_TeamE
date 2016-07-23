3cfa96770543f18a6888048752514304254a9fdd
asakurayonik <526391899@qq.com>
2016-07-22 21:47:04 -0400

    XML added
    Refactor GUI

--- src/User_Interface/GUI.java
+++ src/User_Interface/GUI.java
@@ -23,7 +23,8 @@ public class GUI {
 
 	private JFrame frame;
 	private JTextField textField_R;
-	private JTextArea textArea;
+	private JTextArea textAreaResult;
+	private JTextArea textAreaXML;
 	private JButton btnCalculate;
 	private JButton btnExit;
 	private JTextField textField_Precision;
@@ -67,16 +68,16 @@ public class GUI {
 	 */
 	private void initialize() {
 		frame = new JFrame("CHEERS (Team-E)");
-		frame.setBounds(100, 100, 400, 300);
+		frame.setBounds(100, 100, 600, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		btnCalculate = new JButton("Calculate");
-		btnCalculate.setBounds(10, 186, 174, 31);
+		btnCalculate.setBounds(10, 440, 174, 50);
 		frame.getContentPane().add(btnCalculate);
 
 		btnExit = new JButton("Exit");
-		btnExit.setBounds(10, 220, 174, 31);
+		btnExit.setBounds(10, 501, 174, 50);
 		frame.getContentPane().add(btnExit);
 
 		JLabel lblInput = new JLabel("Input:");
@@ -94,9 +95,13 @@ public class GUI {
 		frame.getContentPane().add(textField_R);
 		textField_R.setColumns(10);
 
-		textArea = new JTextArea();
-		textArea.setBounds(202, 11, 160, 240);
-		frame.getContentPane().add(textArea);
+		textAreaResult = new JTextArea();
+		textAreaResult.setBounds(10, 219, 174, 210);
+		frame.getContentPane().add(textAreaResult);
+		
+		textAreaXML = new JTextArea();
+		textAreaXML.setBounds(194, 42, 380, 509);
+		frame.getContentPane().add(textAreaXML);
 
 		JLabel lblParameter = new JLabel("Parameter:");
 		lblParameter.setFont(new Font("Tahoma", Font.PLAIN, 16));
@@ -124,6 +129,17 @@ public class GUI {
 		frame.getContentPane().add(textField_Tolerance);
 		textField_Tolerance.setColumns(10);
 		textField_Tolerance.setText("0.0001");	//default tolerance
+		
+		JLabel lblResult = new JLabel("Result:");
+		lblResult.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblResult.setBounds(10, 186, 63, 22);
+		frame.getContentPane().add(lblResult);
+		
+		JLabel lblIncarnationXml = new JLabel("Incarnation 2 XML:");
+		lblIncarnationXml.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblIncarnationXml.setBounds(194, 10, 201, 22);
+		frame.getContentPane().add(lblIncarnationXml);
+		
 
 		// ------------------------ event handler -----------------------
 		btnCalculate.addMouseListener(new MouseAdapter() {
@@ -146,38 +162,38 @@ public class GUI {
 						int p = Integer.parseInt(s1);
 						double t = Double.parseDouble(s2);
 						double R = Double.parseDouble(s);
-						textArea.setText("R = " + R + "\nPrecision = " + p + "\nTolerance = " + t + "\n\n");
-				
-						
 						double result1 = setMath(new Incarnation1(p,t)).getL(R);
 						s = Double.toString(result1);
-						textArea.append("Incarnation 1:\n" + s + "\n");
+						textAreaResult.setText("Incarnation 1:\n" + s + "\n");
 						long tEnd = System.currentTimeMillis(); // end time
 						long tDelta = tEnd - tStart;
 						double elapsedSeconds = tDelta / 1000.0;
-						textArea.append("Used " + elapsedSeconds + " seconds.\n");
+						textAreaResult.append("Used " + elapsedSeconds + " seconds.\n\n");
 						// -------------------------------------------------------
 						
 						// -------------- Computing Incarnation 2 ----------------
-						tStart = System.currentTimeMillis(); // start time
-						
+						tStart = System.currentTimeMillis(); // start time	
 						double result2 = setMath(new Incarnation2(t,p)).getL(R);
 						s = Double.toString(result2);
-						textArea.append("Incarnation 2:\n" + s + "\n");
+						textAreaResult.append("Incarnation 2:\n" + s + "\n");
 						tEnd = System.currentTimeMillis(); // end time
 						tDelta = tEnd - tStart;
 						elapsedSeconds = tDelta / 1000.0;
-						XMLWriter xmlOutput = new XMLWriter(result2,elapsedSeconds);
-						textArea.append("Used " + elapsedSeconds + " seconds.\n");
-						textArea.append(xmlOutput.WriteXML());
+						textAreaResult.append("Used " + elapsedSeconds + " seconds.\n\n");
+						
 						// -------------------------------------------------------
 						
 						// -------------- Error Check ----------------
 						double relativeError = Math.abs(result1 - result2) / result2;
-						textArea.append("Relative Error:\n" + relativeError + "\n");
+						textAreaResult.append("Relative Error:\n" + relativeError + "\n");
+						
+						// -------------- XML ----------------
+						XMLWriter xmlOutput = new XMLWriter(result2,elapsedSeconds);
+						textAreaXML.setText("Used " + elapsedSeconds + " seconds.\n");
+						textAreaXML.append(xmlOutput.WriteXML());
 						
 					} else {
-						textArea.setText(
+						textAreaResult.setText(
 								"Notice:\nR: Positive Number\nPrecision: From 1 to 99\nTolerance: Positive Number");
 					}
 				}
--- src/team_E_library/GUI.java
+++
@@ -1,5 +0,0 @@
-package team_E_library;
-
-public class GUI {
-
-}
